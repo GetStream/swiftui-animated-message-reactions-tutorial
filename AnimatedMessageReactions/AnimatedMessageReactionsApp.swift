ef6a0d4d1c0a5a596cc80b8c6f8914eea68ec5a8
Stefan Blos <stefan.blos@gmail.com>
2021-12-14 13:49:36 +0100

    Initial Commit

---
+++ AnimatedMessageReactions/AnimatedMessageReactionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedMessageReactionsApp.swift
+//  AnimatedMessageReactions
+//
+//  Created by Stefan Blos on 14.12.21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedMessageReactionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
