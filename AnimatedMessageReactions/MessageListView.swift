83fd54b8ea21479b768c09c15ac9960bb17f2c58
Stefan Blos <stefan.blos@gmail.com>
2021-12-14 22:41:24 +0100

    Added UI for container view

--- AnimatedMessageReactions/MessageListView.swift
+++ AnimatedMessageReactions/MessageListView.swift
@@ -13,6 +13,17 @@ struct MessageListView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 28)
+                    .fill(Color(UIColor.tertiarySystemGroupedBackground))
+                    .frame(width: 216, height: 40)
+                    .scaleEffect(showReactionsBackground ? 1 : 0, anchor: .bottomTrailing)
+                    .animation(
+                        .interpolatingSpring(stiffness: 170, damping: 15).delay(0.05),
+                        value: showReactionsBackground
+                    )
+            }
+            
             MessageView(person: "lando", text: "Have you checked out the Stream iOS and SwiftUI SDK's? I hear they are incredible!")
         }
         .padding()
